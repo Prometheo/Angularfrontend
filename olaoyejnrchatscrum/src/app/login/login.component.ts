732800e8144ef9f79344b838f7f32b8c27ec5e80
[prometheus] <[olaoyejnr@gmail.com]>
2020-02-13 23:09:24 +0100

    first angular commit, with sign up and login form

---
+++ olaoyejnrchatscrum/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Scrumuser } from '../scrumuser';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  scrumUserModel = new Scrumuser('johndoe@gmail.com','','prometheus','');
+
+}
---
+++ olaoyejnrchatscrum/src/app/signup/signup.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Scrumuser } from '../scrumuser';
+@Component({
+  selector: 'app-signup',
+  templateUrl:'./signup.component.html', 
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  userTypes = ["Developer", "Owner"];
+  scrumUserModel = new Scrumuser('','louis','popson@1996','Developer');
+}
